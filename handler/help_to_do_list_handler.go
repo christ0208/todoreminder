1a6b750692edc7d994cf5a926ba5beeac2ed961a
christ0208 <christopherlimawan@gmail.com>
2021-03-06 20:22:01 +0700

    Changed help format (credits to Chelsey)

--- handler/help_to_do_list_handler.go
+++ handler/help_to_do_list_handler.go
@@ -6,14 +6,13 @@ import (
 )
 
 var (
-	helpInstructions =
-		"To Do Reminder\n" +
+	helpInstructions = "To Do Reminder\n" +
 		"==============\n" +
 		"Command Format: [options]\n" +
 		"Options:\n" +
 		"/subscribe -> Activate Daily Reminder every 07.00 GMT +7\n" +
 		"/unsubscribe -> Deactivate Daily Reminder\n" +
-		"/add [deadline in yyyy-mm-dd] [task name] -> Add new To Do List\n" +
+		"/add [deadline (format: yyyy-mm-dd)] [task name] -> Add new To Do List\n" +
 		"/list -> Show 8 near deadline To Do Lists\n" +
 		"/list [date in yyyy-mm-dd] -> Show all To Do Lists on that date\n" +
 		"/remove [To Do ID] -> Remove To Do List based on ID\n" +
@@ -24,7 +23,7 @@ var (
 		"Hardwork and Extraordinary Effort Makes Success Comes Closer ~ Bluejack 18-1"
 )
 
-type HelpToDoListHandler struct {}
+type HelpToDoListHandler struct{}
 
 func (handler HelpToDoListHandler) Handle(bot *linebot.Client, event *linebot.Event) {
 	_, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(helpInstructions)).Do()
